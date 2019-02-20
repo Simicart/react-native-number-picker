b665bea6faa7a4f86d26649d1715c0a156816b8e
Glenn <glenn@simicart.com>
2019-02-20 14:50:36 +0700

    Update library for ios

--- ios/RNNumberPickerLibrary.m
+++ ios/RNNumberPickerLibrary.m
@@ -1,13 +1,50 @@
 
 #import "RNNumberPickerLibrary.h"
+#import "ActionSheetPicker.h"
 
-@implementation RNNumberPickerLibrary
+@implementation RNNumberPickerLibrary{NSMutableArray *qtyArray; RCTResponseSenderBlock onDoneClick; RCTResponseSenderBlock onCancelClick;}
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(createDialog:(NSDictionary *)indic createDialog:(RCTResponseSenderBlock)doneCallback createDialog:(RCTResponseSenderBlock)cancelCallback){
+    int minValue=[indic[@"minValue"] intValue];
+    int maxValue=[indic[@"maxValue"] intValue];
+    onDoneClick = doneCallback;
+    onCancelClick = cancelCallback;
+    //  NSString *doneText=indic[@"doneText"];
+    //  NSString *doneTextColor=indic[@"doneTextColor"];
+    //  NSString *cancelText=indic[@"cancelText"];
+    //  NSString *cancelTextColor=indic[@"cancelTextColor"];
+    
+    qtyArray = [[NSMutableArray alloc] init];
+    for (int i = minValue; i <= maxValue; i+= 1) {
+        [qtyArray addObject:[NSString stringWithFormat:@"%d",i]];
+    }
+    int selectedValue = 0;
+    if(indic[@"selectedValue"]) {
+        selectedValue = [indic[@"selectedValue"] intValue];
+    }
+    
+    ActionSheetStringPicker* qtyPicker = [[ActionSheetStringPicker alloc]initWithTitle:@"" rows:qtyArray initialSelection:selectedValue target:self successAction:@selector(didSelectValue:element:) cancelAction:@selector(cancelActionSheet:) origin:self];
+    [qtyPicker showActionSheetPicker];
+}
+
+- (void)didSelectValue:(NSNumber *)selectedIndex element:(id)element
+{
+    int qty = [[qtyArray objectAtIndex:[selectedIndex intValue]]intValue];
+    NSString *qtyString = [NSString stringWithFormat:@"%i", qty];
+    NSArray *events = @[qtyString];
+    onDoneClick(@[[NSNull null], events]);
+}
+
+-(void) cancelActionSheet:(id)sender{
+    onCancelClick(@[[NSNull null], nil])
+}
 
 @end
   
